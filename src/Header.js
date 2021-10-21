f8913770ed41cb2da733e69a6e11ad87f55ae2c5
Gurprem Bhullar <bhullargurprem@gmail.com>
2021-10-21 15:16:27 -0400

    just did some debugging

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,41 @@
 import React from 'react'
+import HeaderOption from './HeaderOption';
 import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import Linkedin from "./linkedin.jpg";
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+
 
 function Header() {
     return (
         <div className='header'>
-            <h1>This is the Header</h1>
+
+
+            <div className="header__left">
+                <img src={Linkedin} alt="Linkedin " />
+
+                <div className="header__search">
+                    <SearchIcon />
+                    <input type="text" />
+                </div>
+
+            </div>
+
+            <div className="header__right">
+                <HeaderOption Icon={HomeIcon} title="Home" />
+                <HeaderOption Icon={SupervisorAccountIcon} title="SupervisorAccount" />
+                <HeaderOption Icon={BusinessCenterIcon} title="BusinessCenter" />
+                <HeaderOption Icon={ChatIcon} title="Chat" />
+                <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+            </div>
 
         </div>
     )
 }
 
-export default Header 
+export default Header
