88c7754bf45e162b865050250b6b8ea15702aef3
Gurprem Bhullar <bhullargurprem@gmail.com>
2021-10-29 20:56:23 -0400

    made the sidebar nicer

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./Sidebar.css";
 import { Avatar } from '@mui/material';
+import Background from "./background.jpg";
 
 function Sidebar() {
-    return <div className="sidebar">
+    return (
+    <div className="sidebar">
         <div className="sidebar__top">
-            <img src="" alt="" />
+            <img src={Background} alt="background" />
             <Avatar className="sidebar__avatar" />
             <h2>Gurprem Bhullar</h2>
             <h4>bhullargurprem@gmail.com</h4>
@@ -17,12 +19,16 @@ function Sidebar() {
                 <p className="sidebar__statNumber">2,543</p>
             </div>
             <div className="sidebar__stat">
-            <p>Views on post</p>
-            <p className="sidebar__statNumber">2,448</p>
+                <p>Views on post</p>
+                <p className="sidebar__statNumber">2,448</p>
             </div>
         </div>
-    </div>;
 
+        <div className="sidebar__bottom">
+            <p>Recent</p>
+        </div>
+    </div>
+    );
 }
 export default Sidebar;
 
