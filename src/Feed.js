832fc060c7097b4a584c597d3bba2adea411cda4
Gurprem Bhullar <bhullargurprem@gmail.com>
2021-10-30 19:35:13 -0400

    added material ui icons

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import "./Feed.css";
 import CreateIcon from '@mui/icons-material/Create';
+import InputOption from "./InputOption";
+import ImageIcon from '@mui/icons-material/Image';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
 
 function Feed() {
     return (
@@ -8,9 +13,18 @@ function Feed() {
             <div className="feed__inputContainer">
                 <div className="feed__input">
                     <CreateIcon />
+                    <form>
+                        <input type="text" />
+                        <button type="submit">Get Gurped</button>
+                    </form>
+                </div>
+                <div className="feed__inputOptions">
+                    <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+                    <InputOption Icon={SubscriptionsIcon} title="Photo" color="#E7A33E" />
+                    <InputOption Icon={EventNoteIcon} title="Photo" color="#C0CBCD" />
+                    <InputOption Icon={CalendarViewDayIcon} title="Photo" color="#7FC15E" />
                 </div>
             </div>
-            
         </div>
     )
 }
